d22d3a71c84d43331c64631b2e2a885fc773a6a6
xomf <xomf@the-atf-shot-my.dog>
2024-03-23 16:06:04 -0400

    silly

--- src/public/mod.rs
+++ src/public/mod.rs
@@ -5,7 +5,7 @@ mod templates;
 use actix_web::{web, App, HttpServer};
 
 pub(crate) async fn start_actix() -> anyhow::Result<()> {
-    log::info!("Serving an http server at https://0.0.0.0:8080");
+    log::info!("Serving an http server at http://0.0.0.0:8080");
     HttpServer::new(|| {
         App::new()
             .route("/", web::get().to(routes::index))
