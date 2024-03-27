c55d6d37e25dae92140de9cb774f1baf0c9de27e
MCorange99 <mcorange@mcorangehq.xyz>
2024-03-27 23:19:07 +0200

    initial html changes

--- src/public/mod.rs
+++ src/public/mod.rs
@@ -15,7 +15,8 @@ pub(crate) async fn start_actix(config: &Config, database: Database) -> anyhow::
         App::new()
             .app_data(actix_web::web::Data::new(database.clone()))
             .route("/", web::get().to(routes::index)) // index.html
-            .service(actix_fs::Files::new("/static", "./static").index_file("index.html")) // static directoryh
+            .service(actix_fs::Files::new("/static", "./static").index_file("index.html")) // static directory
+            .service(web::redirect("/favicon.ico", "/static/favicon.ico")) //? special redirect for favicon
     })
 
     .bind(bindip)?
